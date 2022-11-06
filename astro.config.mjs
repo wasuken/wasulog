11ea631a6b4da04886ec6a503dad02f9645ebb8a
wasuken <wevorence@gmail.com>
2022-11-06 18:55:34 +0900

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,12 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  site: 'https://wasu-report.netlify.app',
+  integrations: [mdx(), sitemap(), react()],
+  output: "server",
+  adapter: netlify()
 });
