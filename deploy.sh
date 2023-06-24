957a4471b7bf49db255eb6a2d83c00c64a51ff02
wasuken <wevorence@gmail.com>
2023-06-25 08:15:49 +0900

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+dir=`pwd`
+
 cd ~/memo/tools/gen_report
 bundle exec ruby main.rb
 cp -Rf ~/memo/tools/gen_report/output/* ~/dev/wasulog/src/pages/blog/
 
+cd $dir
 git add .
 git commit -m "commit"
 git push origin main
